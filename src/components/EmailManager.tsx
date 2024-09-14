8aaa6460583cbb7aee7182c316b827c8e5b43c47
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2024-09-14 21:37:02 +0300

    updated to two lists view

--- src/components/EmailManager.tsx
+++ src/components/EmailManager.tsx
@@ -8,8 +8,8 @@ const EmailManager: React.FC = () => {
     const [selectedRecipients, setSelectedRecipients] = useState<Recipient[]>([]);
     const [inputValue, setInputValue] = useState<string>("");
     const [suggestions, setSuggestions] = useState<string[]>([]);
-    const [inputValueRecipients, setInputValueRecipients] = useState<string>("");
-    const [suggestionsRecipients, setSuggestionsRecipients] = useState<string[]>([]);
+    // const [inputValueRecipients, setInputValueRecipients] = useState<string>("");
+    // const [suggestionsRecipients, setSuggestionsRecipients] = useState<string[]>([]);
     const [inputValueDomains, setInputValueDomains] = useState<string>("");
     const [suggestionsDomains, setSuggestionsDomains] = useState<string[]>([]);
     const [inputValueSelectedRecs, setInputValueSelectedRecs] = useState<string>("");
@@ -35,10 +35,10 @@ const EmailManager: React.FC = () => {
 
     const handleSuggestionClick = (suggestion: string) => {
         setInputValue(suggestion);
-        setSuggestions([]); 
+        setSuggestions([]);
     };
 
-    const sortedRecipients = recipients.sort((a,b) => {
+    const sortedRecipients = recipients.sort((a, b) => {
         const companyA = a.email.split("@")[1].toLowerCase();
         const companyB = b.email.split("@")[1].toLowerCase();
         return companyA.localeCompare(companyB);
@@ -62,96 +62,89 @@ const EmailManager: React.FC = () => {
         }
     };
 
-     // Recipients arama fonksiyonu
-  const handleInputChangeRecipients = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setInputValueRecipients(value);
-    if (value === "") {
-      setSuggestionsRecipients([]);
-      return;
-    }
+    // const handleInputChangeRecipients = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     const value = e.target.value;
+    //     setInputValueRecipients(value);
+    //     if (value === "") {
+    //         setSuggestionsRecipients([]);
+    //         return;
+    //     }
 
-    const filteredSuggestions = recipients
-      .map(recipient => recipient.email)
-      .filter((email, index, self) => self.indexOf(email) === index)
-      .filter(email => email.toLowerCase().startsWith(value.toLowerCase()));
+    //     const filteredSuggestions = recipients
+    //         .map(recipient => recipient.email)
+    //         .filter((email, index, self) => self.indexOf(email) === index)
+    //         .filter(email => email.toLowerCase().startsWith(value.toLowerCase()));
 
-    setSuggestionsRecipients(filteredSuggestions);
-  };
+    //     setSuggestionsRecipients(filteredSuggestions);
+    // };
 
-  const handleSuggestionClickRecipients = (suggestion: string) => {
-    setInputValueRecipients(suggestion);
-    const selectedRecipient = recipients.find(r => r.email === suggestion);
-    if(selectedRecipient && !selectedRecipients.find(r => r.email === selectedRecipient.email)){
-        setSelectedRecipients([...selectedRecipients, selectedRecipient]);
-    }
+    const handleSuggestionClickDomains = (suggestion: string) => {
+        setInputValueDomains(suggestion);
+        const selectedDomain = recipients.find(r => r.domain === suggestion);
+        if (selectedDomain && !selectedRecipients.find(r => r.domain === selectedDomain.domain)) {
+            setSelectedRecipients([...selectedRecipients, selectedDomain]);
+        }
 
-    setSuggestionsRecipients([]);
-  };
+        setSuggestionsDomains([]);
+    };
 
-  // Domains arama fonksiyonu
-  const handleInputChangeDomains = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setInputValueDomains(value);
-    if (value === "") {
-      setSuggestionsDomains([]);
-      return;
-    }
 
-    const filteredSuggestions = recipients
-      .map(recipient => recipient.domain)
-      .filter((domain, index, self) => self.indexOf(domain) === index)
-      .filter(domain => domain.toLowerCase().startsWith(value.toLowerCase()));
+    const handleInputChangeDomains = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setInputValueDomains(value);
+        if (value === "") {
+            setSuggestionsDomains([]);
+            return;
+        }
 
-    setSuggestionsDomains(filteredSuggestions);
-  };
+        const filteredSuggestions = recipients
+            .map(recipient => recipient.domain)
+            .filter((domain, index, self) => self.indexOf(domain) === index)
+            .filter(domain => domain.toLowerCase().startsWith(value.toLowerCase()));
 
-  const handleSuggestionClickDomains = (suggestion: string) => {
-    setInputValueDomains(suggestion);
-    setSuggestionsDomains([]); 
-  };
+        setSuggestionsDomains(filteredSuggestions);
+    };
 
-  
-  const handleInputChangeSelectedRecipients = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setInputValueSelectedRecs(value);
-    if (value === "") {
-      setSuggestionsSelectedRecs([]);
-      return;
-    }
+    const handleInputChangeSelectedRecipients = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setInputValueSelectedRecs(value);
+        if (value === "") {
+            setSuggestionsSelectedRecs([]);
+            return;
+        }
 
-    const filteredSuggestions = selectedRecipients
-      .map(recipient => recipient.email)
-      .filter((email, index, self) => self.indexOf(email) === index)
-      .filter(email => email.toLowerCase().startsWith(value.toLowerCase()));
+        const filteredSuggestions = selectedRecipients
+            .map(recipient => recipient.email)
+            .filter((email, index, self) => self.indexOf(email) === index)
+            .filter(email => email.toLowerCase().startsWith(value.toLowerCase()));
 
-    setSuggestionsSelectedRecs(filteredSuggestions);
-  };
+        setSuggestionsSelectedRecs(filteredSuggestions);
+    };
 
-  const handleSuggestionClickSelectedRecipients = (suggestion: string) => {
-    setInputValueSelectedRecs(suggestion);
-    setSuggestionsSelectedRecs([]); 
-  };
+    const handleSuggestionClickSelectedRecipients = (suggestion: string) => {
+        setInputValueSelectedRecs(suggestion);
+        setSuggestionsSelectedRecs([]);
+    };
 
-  // Selected Domains arama fonksiyonu
-  const handleInputChangeSelectedDomains = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setInputValueSelectedDoms(value);
-    if (value === "") {
-      setSuggestionsSelectedDoms([]);
-      return;
-    }
 
-    const filteredSuggestions = Array.from(new Set(selectedRecipients.map(r => r.domain)))
-      .filter(domain => domain.toLowerCase().startsWith(value.toLowerCase()));
+    const handleInputChangeSelectedDomains = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setInputValueSelectedDoms(value);
+        if (value === "") {
+            setSuggestionsSelectedDoms([]);
+            return;
+        }
 
-    setSuggestionsSelectedDoms(filteredSuggestions);
-  };
+        const filteredSuggestions = Array.from(new Set(selectedRecipients.map(r => r.domain)))
+            .filter(domain => domain.toLowerCase().startsWith(value.toLowerCase()));
 
-  const handleSuggestionClickSelectedDomains = (suggestion: string) => {
-    setInputValueSelectedDoms(suggestion);
-    setSuggestionsSelectedDoms([]); 
-  };
+        setSuggestionsSelectedDoms(filteredSuggestions);
+    };
+
+    const handleSuggestionClickSelectedDomains = (suggestion: string) => {
+        setInputValueSelectedDoms(suggestion);
+        setSuggestionsSelectedDoms([]);
+    };
 
     const handleSelectRecipient = (recipient: Recipient): void => {
         if (!selectedRecipients.find(r => r.email === recipient.email)) {
@@ -200,52 +193,20 @@ const EmailManager: React.FC = () => {
             <button className="ms-2 btn btn-primary rounded-pill" onClick={handleEmailAdd}>Add Email</button>
             <Container>
                 <Row className="mt-4">
-                    <Col xs={12} md={{span:5,offset:1}} className="border border-2 border-grey pt-2 rounded shadow p-3 mb-5 bg-white rounded" >
-                        <FormGroup >
-                            <FormLabel>Search in Available Email Recipients</FormLabel>
-                            <Form.Control
-                            style={{maxWidth:"fit-content",marginLeft:"auto",marginRight:"auto"}}
-                                type="text"
-                                value={inputValueRecipients}
-                                onChange={handleInputChangeRecipients}
-                                placeholder="Search An Email " />
-                            {suggestionsRecipients.length > 0 && (
-                                <ListGroup>
-                                    {suggestionsRecipients.map((suggestion, index) => (
-                                        <ListGroup.Item
-                                            key={index}
-                                            action
-                                            onClick={() => handleSuggestionClickRecipients(suggestion)}
-                                        >
-                                            {suggestion}
-                                        </ListGroup.Item>
-                                    ))}
-                                </ListGroup>
-                            )}
-                            
-                        </FormGroup>
-
-                        <h3>Available Email Recipients</h3>
-                        <ul>
-                            {sortedRecipients.map((recipient, index) => (
-                                <li className="d-flex justify-content-between my-2" key={index}>
-                                    {recipient.email}{" "}
-                                    <button className="btn btn-success m-1" onClick={() => handleSelectRecipient(recipient)}>
-                                        Select
-                                    </button>
-                                </li>
-                            ))}
-                        </ul>
-                    </Col>
-                    <Col xs={12} md={{span:5,offset:1}} className="border border-2 border-grey pt-2 rounded shadow p-3 mb-5 bg-white rounded" >
+                    <Col
+                        xs={12}
+                        md={{ span: 5, offset: 1 }}
+                        className="border border-2 border-grey pt-2 rounded shadow p-3 mb-5 bg-white rounded"
+                    >
                         <FormGroup>
-                            <FormLabel>Search in Available Companies</FormLabel>
+                            <FormLabel>Search in Available Email Recipients and Companies</FormLabel>
                             <Form.Control
-                            style={{maxWidth:"fit-content",marginLeft:"auto",marginRight:"auto"}}
+                                style={{ maxWidth: "fit-content", marginLeft: "auto", marginRight: "auto" }}
                                 type="text"
                                 value={inputValueDomains}
                                 onChange={handleInputChangeDomains}
-                                placeholder="Search A Company Name" />
+                                placeholder="Search Email or Company"
+                            />
                             {suggestionsDomains.length > 0 && (
                                 <ListGroup>
                                     {suggestionsDomains.map((suggestion, index) => (
@@ -259,35 +220,51 @@ const EmailManager: React.FC = () => {
                                     ))}
                                 </ListGroup>
                             )}
-                            
                         </FormGroup>
-                        <h3>Available Company Recipients</h3>
+
+                        <h3>Available Recipients by Company</h3>
                         <ul>
-                        {Array.from(new Set(sortedRecipients.map(r => r.domain))).map(
-                            (domain, index) => (
-                                <li className="d-flex justify-content-between my-2" key={index}>
-                                    {domain}
-                                    <button className="btn btn-success m-1" onClick={() => handleSelectDomain(domain)}>
+                            {Array.from(new Set(sortedRecipients.map((r) => r.domain))).map((domain, index) => (
+                                <div key={index} className="my-3">
+                                    <strong>{domain}</strong>
+                                    <ul>
+                                        {sortedRecipients
+                                            .filter((recipient) => recipient.domain === domain)
+                                            .map((recipient, subIndex) => (
+                                                <li className="d-flex justify-content-between my-2" key={subIndex}>
+                                                    {recipient.email}
+                                                    <button
+                                                        className="btn btn-success btn-sm m-1"
+                                                        onClick={() => handleSelectRecipient(recipient)}
+                                                    >
+                                                        Select Email
+                                                    </button>
+                                                </li>
+                                            ))}
+                                    </ul>
+                                    <button
+                                        className="btn btn-primary btn-sm my-2 "
+                                        onClick={() => handleSelectDomain(domain)}
+                                    >
                                         Select Domain
                                     </button>
-                                </li>
-                            )
-                        )}
+                                </div>
+                            ))}
                         </ul>
                     </Col>
-                </Row>
-                <Row className="mt-4">
-                <Col xs={12} md={{span:5,offset:1}} className="border border-2 border-grey pt-2 rounded shadow p-3 mb-5 bg-white rounded" >
+
+
+                    <Col xs={12} md={{ span: 5, offset: 1 }} className="border border-2 border-grey pt-2 rounded shadow p-3 mb-5 bg-white rounded" >
                         <FormGroup>
                             <FormLabel>Search in Selected Email Recipients</FormLabel>
                             <Form.Control
-                            style={{maxWidth:"fit-content",marginLeft:"auto",marginRight:"auto"}}
+                                style={{ maxWidth: "fit-content", marginLeft: "auto", marginRight: "auto" }}
                                 type="text"
                                 value={inputValueSelectedRecs}
                                 onChange={handleInputChangeSelectedRecipients}
                                 placeholder="Search An Email" />
                             {suggestionsSelectedRecs.length > 0 && (
-                                <ListGroup>
+                                <ListGroup className="mb-4 min-vh-25">
                                     {suggestionsSelectedRecs.map((suggestion, index) => (
                                         <ListGroup.Item
                                             key={index}
@@ -299,25 +276,25 @@ const EmailManager: React.FC = () => {
                                     ))}
                                 </ListGroup>
                             )}
-                            
+
                         </FormGroup>
                         <h3>Selected Email Recipients</h3>
-                <ul>
-                    {selectedRecipients.map((recipient, index) => (
-                        <li className="d-flex justify-content-between my-2" key={index}>
-                            {recipient.email}{" "}
-                            <button className="btn btn-danger m-1" onClick={() => handleRemoveRecipient(recipient)}>
-                                Remove
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-                    </Col>
-                    <Col xs={12} md={{span:5,offset:1}} className="border border-2 border-grey pt-2 rounded shadow p-3 mb-5 bg-white rounded" >
-                        <FormGroup>
+                        <ul>
+                            {selectedRecipients.map((recipient, index) => (
+                                <li className="d-flex justify-content-between my-2" key={index}>
+                                    {recipient.email}{" "}
+                                    <button className="btn btn-danger m-1" onClick={() => handleRemoveRecipient(recipient)}>
+                                        Remove
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+
+
+                        <FormGroup className="mt-4 pt-4 ">
                             <FormLabel>Search in Selected Companies</FormLabel>
                             <Form.Control
-                            style={{maxWidth:"fit-content",marginLeft:"auto",marginRight:"auto"}}
+                                style={{ maxWidth: "fit-content", marginLeft: "auto", marginRight: "auto" }}
                                 type="text"
                                 value={inputValueSelectedDoms}
                                 onChange={handleInputChangeSelectedDomains}
@@ -335,21 +312,21 @@ const EmailManager: React.FC = () => {
                                     ))}
                                 </ListGroup>
                             )}
-                            
+
                         </FormGroup>
                         <h3>Selected Company Recipients</h3>
-                <ul>
-                    {Array.from(new Set(selectedRecipients.map(r => r.domain))).map(
-                        (domain, index) => (
-                            <li className="d-flex justify-content-between my-2" key={index}>
-                                {domain}
-                                <button className="btn btn-danger m-1" onClick={() => handleRemoveDomain(domain)}>
-                                    Remove Domain
-                                </button>
-                            </li>
-                        )
-                    )}
-                </ul>
+                        <ul>
+                            {Array.from(new Set(selectedRecipients.map(r => r.domain))).map(
+                                (domain, index) => (
+                                    <li className="d-flex justify-content-between my-2" key={index}>
+                                        {domain}
+                                        <button className="btn btn-danger m-1" onClick={() => handleRemoveDomain(domain)}>
+                                            Remove Domain
+                                        </button>
+                                    </li>
+                                )
+                            )}
+                        </ul>
                     </Col>
                 </Row>
             </Container>
